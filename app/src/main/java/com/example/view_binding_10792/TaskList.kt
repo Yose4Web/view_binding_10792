dcfdebb412d8da97eeddd8b64e536e8f8241b12a
Yose4Web <yoseforweb@gmail.com>
2022-09-15 08:51:27 +0700

    view_binding_10792

---
+++ app/src/main/java/com/example/view_binding_10792/TaskList.kt
@@ -0,0 +1,11 @@
+package com.example.view_binding_10792
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Chapter 1", "Buka Valo", "Kebiasaan di malam yg gabut"),
+        Task("Chapter 2", "Main Unrated", "1 game bersama para random"),
+        Task("Chapter 3", "Ketemu teammate burik", "Alasannya selalu karena nge-lag"),
+        Task("Chapter 4", "Dibantai team musuh", "Mereka mengatakan GG EZ"),
+        Task("Chapter 5", "Menutup valo", "Demi kesehatan mental")
+    )
+}
